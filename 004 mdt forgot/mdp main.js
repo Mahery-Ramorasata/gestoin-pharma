596b346228581b6c8f9b3a5ace8cd314ea0a9aa4
Mahery-Ramorasata <134946935+Mahery-Ramorasata@users.noreply.github.com>
2025-07-18 10:56:34 +0300

    Add files via upload

---
+++ 004 mdt forgot/mdp main.js
@@ -0,0 +1,9 @@
+ const slider = document.getElementById("slider");
+
+    function showRecovery() {
+      slider.classList.add("active");
+    }
+
+    function hideRecovery() {
+      slider.classList.remove("active");
+    }
